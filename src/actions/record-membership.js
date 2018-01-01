fcda55137788477efb5b9692dae6bef2ba008966
Kris Harbold <kris@chartbeat.com>
2017-12-31 22:42:20 -0500

    Membership records

--- src/actions/record-membership.js
+++ src/actions/record-membership.js
@@ -8,12 +8,15 @@ es6PromisePolyfill.polyfill();
 const GAPI_URL = 'https://script.google.com/a/climbcrux.org/macros/s/AKfycbzhTwPAhCcXeTz00yqUQsQPYU4_DEd8nXtRwA-B7PJQIZSTYOpn/exec';
 
 
-export const WRITE_SUCCESS = 'GAPI_WRITE_SUCCESS';
+export const WRITE_SUCCESS = 'MEMBERHSHIP_WRITE_SUCCESS';
 const writeSuccess = (data) => {
-  return {
-    type: WRITE_SUCCESS,
-    payload: null,
-  };
+  return {type: WRITE_SUCCESS};
+};
+
+
+export const WRITE_FAILURE = 'MEMBERSHIP_WRITE_FAILURE';
+const writeFailure = () => {
+  return {type: WRITE_FAILURE};
 };
 
 
@@ -24,12 +27,12 @@ export const writeMembership = (data) => {
 
     return fetch(url, {method: 'GET'}).then(response => {
       if (response.status > 400) {
-        console.log('That didnt work');
+        dispatch(writeFailure);
       } else {
         dispatch(writeSuccess);
       }
     }).catch(error => {
-      console.log(error);
+      dispatch(writeFailure);
     });
 	}
 };
--- src/reducer/membership-level.js
+++ src/reducer/membership-level.js
@@ -1,9 +1,11 @@
 import { CHANGE_LEVEL } from '../container/join/join';
+import { WRITE_SUCCESS, WRITE_FAILURE } from '../actions/record-membership';
 
 
 const initState = {
   level: 'standard',
   price: '$50',
+  writeSuccess: null,
 };
 
 export default function Membership(state=initState, action) {
@@ -12,6 +14,16 @@ export default function Membership(state=initState, action) {
       return {
         ...state,
         ...action.payload,
+      }
+    case WRITE_SUCCESS:
+      return {
+        ...state,
+        writeSuccess: true
+      }
+    case WRITE_FAILURE:
+      return {
+        ...state,
+        writeSuccess: false,
       }
     default:
       return state;
