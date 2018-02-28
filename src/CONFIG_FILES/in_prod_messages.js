b9cd535ff4c861f8164426b113228a4798d12f29
Kris Harbold <kris@chartbeat.com>
2018-02-27 23:54:12 -0500

    Fix typo in IPM

--- src/CONFIG_FILES/in_prod_messages.js
+++ src/CONFIG_FILES/in_prod_messages.js
@@ -3,6 +3,6 @@ import React from 'react';
 
 export const Message = (
   <div>
-    We hope you are enjoying the new CRUX website. <br/>Find an issue or have feedback? Drop us at note at <a href="mailto:web@climbcrux.org" target="_blank">web@climbcrux.org</a>.
+    We hope you are enjoying the new CRUX website. <br/>Find an issue or have feedback? Drop us a note at <a href="mailto:web@climbcrux.org" target="_blank">web@climbcrux.org</a>.
   </div>
 );
