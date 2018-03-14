2c950df1fc4cdba3bd264e2fd2592a2fba1a6c40
Kris Harbold <kris@chartbeat.com>
2018-03-14 00:23:15 -0400

    Another new homepage photo

--- src/CONFIG_FILES/photoGallery.js
+++ src/CONFIG_FILES/photoGallery.js
@@ -4,6 +4,7 @@ export const HOMEPAGE_IMAGES = [
   require('../media/homepage/anchorbuilding.jpg'),
   require('../media/homepage/dumbobridge.jpg'),
   require('../media/homepage/pride15.jpg'),
+  require('../media/homepage/climbathon2015.jpg'),
   require('../media/homepage/bouldering.jpg'),
   require('../media/homepage/redrock.jpg'),
   require('../media/homepage/dumbo.jpg'),
