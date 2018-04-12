d25513cf13d596d8d33f98d109f1045585ab299c
Kris Harbold <kris@chartbeat.com>
2018-04-12 12:17:16 -0400

    Change out IPM for membership drive

--- src/CONFIG_FILES/in_prod_messages.js
+++ src/CONFIG_FILES/in_prod_messages.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 export const Message = (
   <div>
-    We hope you are enjoying the new CRUX website. <br/>Find an issue or have feedback? Drop us a note at <a href="mailto:web@climbcrux.org" target="_blank">web@climbcrux.org</a>.
+    Are you a 2018 member yet? <a href="join/">JOIN TODAY</a> during our
+    membership drive to get $10 off your membership. Support our programming 
+    and gain access to exclusive member perks and events.
   </div>
 );
+
+const OLD_MESSAGES = [(
+  <div>
+    We hope you are enjoying the new CRUX website. <br/>Find an issue or have
+    feedback? Drop us a note at <a href="mailto:web@climbcrux.org"
+    target="_blank">web@climbcrux.org</a>.
+  </div>
+)];
