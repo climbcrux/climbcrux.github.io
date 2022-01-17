90c8767c7462f4a4c8b175d886d43ab940ce2367
Kris Harbold <harbolkn@gmail.com>
2022-01-17 10:33:05 -0500

    Update location of Storybook webpack configuration

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,22 @@
-'use strict';
+const path = require('path');
+const config = require('../webpack.config.common.js');
 
-var path = require('path');
+// Additional Loaders
+const cssModules = require('../webpack-config/loader-configurations/cssm');
+const babelStage1React = require('../webpack-config/loader-configurations/babel-stage1-react');
+const image = require('../webpack-config/loader-configurations/image');
 
-const config = require(path.join(__dirname, '../webpack-config/storybook'));
+// Overwrite the module loader rules
+// This is needed to remove the css-loader used in the common config
+config.module.rules = [
+  babelStage1React,
+  cssModules,
+  image,
+  {
+    test: /\.(js|jsx)$/,
+    loader: 'babel-loader',
+    include: [path.join(__dirname, '../src/')]
+  }
+]
+
 module.exports = config;
