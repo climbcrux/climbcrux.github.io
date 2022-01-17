b80c119b9a9c2e7596c500516ac78e665f22f927
Kris Harbold <harbolkn@gmail.com>
2022-01-17 10:51:05 -0500

    Add back css-loader for animation files

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,22 +1,12 @@
 const path = require('path');
 const config = require('../webpack.config.common.js');
 
-// Additional Loaders
-const cssModules = require('../webpack-config/loader-configurations/cssm');
-const babelStage1React = require('../webpack-config/loader-configurations/babel-stage1-react');
-const image = require('../webpack-config/loader-configurations/image');
-
-// Overwrite the module loader rules
-// This is needed to remove the css-loader used in the common config
-config.module.rules = [
-  babelStage1React,
-  cssModules,
-  image,
+config.module.rules = config.module.rules.concat([
   {
     test: /\.(js|jsx)$/,
     loader: 'babel-loader',
     include: [path.join(__dirname, '../src/')]
   }
-]
+])
 
 module.exports = config;
